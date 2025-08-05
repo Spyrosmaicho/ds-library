0d6bb6d392d575a5be4099674a6d59c8726e4e9d
Spyrosmaicho”git config --global user.name Spyrosmaicho” <sdi2400114@di.uoa.gr>
2025-08-05 15:01:35 +0300

    json parser change

--- include/jason.h
+++ include/jason.h
@@ -2,6 +2,6 @@
 #define JASON_H
 
 //Checks if the json file is valid. Returns 0 if valid, 1 otherwise.
-int extraction_mode(char *name_of_file);
+int json_parser(char *name_of_file);
 
 #endif // JASON_H
